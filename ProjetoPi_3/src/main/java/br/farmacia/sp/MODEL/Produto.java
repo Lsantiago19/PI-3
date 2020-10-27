ebf7c122c684ba167ee07e8dbe80b7c9a0713c69
Felipe Canejo <felipecanejo@gmail.com>
2020-10-26 22:35:55 -0300

    Produto e usuário

---
+++ ProjetoPi_3/src/main/java/br/farmacia/sp/MODEL/Produto.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.farmacia.sp.MODEL;
+
+/**
+ *
+ * @author canejo
+ */
+public class Produto {
+    private int codigo;
+    private String descricao;
+    private double preco;
+    private long estoque;
+    private int codigoMarca;
+    private int codigoCategoria;
+    private String unidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public long getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(long estoque) {
+        this.estoque = estoque;
+    }
+
+    public int getCodigoMarca() {
+        return codigoMarca;
+    }
+
+    public void setCodigoMarca(int codigoMarca) {
+        this.codigoMarca = codigoMarca;
+    }
+
+    public int getCodigoCategoria() {
+        return codigoCategoria;
+    }
+
+    public void setCodigoCategoria(int codigoCategoria) {
+        this.codigoCategoria = codigoCategoria;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+    
+    
+    
+}
